8b44d6ba55c84699d24d539a1cf66c9aa6fe89af
Corrie <corrierthomas@gmail.com>
2021-03-31 19:26:05 -0400

    adds appcopy.js to track hw help notes

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,41 @@
 // from data.js
-var tableData = data;
+let tableData = data;
+
+console.log("tableData", tableData);
+
+tableData.forEach(function (item) {
+    console.log(item["datetime"]);
+});
+
+let date = "1/1/2010"
+
+tableData.filter((datum) => {
+    return datum.datetime === date
+})
 
 // YOUR CODE HERE!
+let filteredData = tableData.slice(0,10);
+
+let table = d3.select("tbody");
+
+// tableData.forEach((rec) => {
+//    let tr = table.append("tr");
+//    tr.append("td").text(rec.datetime);
+//    tr.append("td").text(rec.city);
+//    tr.append("td").text(rec.state);
+//    tr.append("td").text(rec.country);
+//    tr.append("td").text(rec.shape);
+//    tr.append("td").text(rec.durationMinutes);
+//    tr.append("td").text(rec.comments);
+// });
+
+d3.select("button").on("click", () => filteredData.forEach((rec) => {
+    let tr = table.append("tr");
+    tr.append("td").text(rec.datetime);
+    tr.append("td").text(rec.city);
+    tr.append("td").text(rec.state);
+    tr.append("td").text(rec.country);
+    tr.append("td").text(rec.shape);
+    tr.append("td").text(rec.durationMinutes);
+    tr.append("td").text(rec.comments);
+}));
