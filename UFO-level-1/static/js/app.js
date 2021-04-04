98cd2c59c1207e6a1c4c4e55d77dd69b8b3c4b9b
Corrie <corrierthomas@gmail.com>
2021-04-03 21:39:24 -0400

    cleans code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,7 +1,7 @@
 // from data.js
 let tableData = data;
 
-console.log("tableData", tableData);
+// console.log("tableData", tableData);
 
 // tableData.forEach(function (item) {
 //     console.log(item["datetime"]);
@@ -29,3 +29,37 @@ tableData.forEach((rec) => {
    tr.append("td").text(rec.durationMinutes);
    tr.append("td").text(rec.comments);
 });
+
+let button = d3.select("#filter-btn");
+let inputfield = d3.select("#datetime");
+
+button.on("click", runSearch); 
+// inputfield.on("click", runSearch);
+
+function runSearch() {
+    d3.event.preventDefault();
+    let inputElement = inputfield.property("value");   
+    let filteredData = tableData.filter(row => row.datetime === inputElement);
+    console.log(filteredData);
+    filteredData.forEach((row) => {
+        // return table.text(row.datetime)
+        // return table.text(row.city)
+        // return table.text(row.state)
+        // return table.text(row.country)
+        // return table.text(row.shape)
+        // return table.text(row.durationMinutes)
+        // return table.text(row.comments);
+        
+        let tr = table.append("tr");
+        tr.append("td").text(row.datetime);
+        tr.append("td").text(row.city);
+        tr.append("td").text(row.state);
+        tr.append("td").text(row.country);
+        tr.append("td").text(row.shape);
+        tr.append("td").text(row.durationMinutes);
+        tr.append("td").text(row.comments);
+    });
+}
+
+
+
